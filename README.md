91158e970848acf3e7f139df438317c5e0d073fa
slysinema <ivan.sidorenko373@gmail.com>
2024-10-16 01:57:33 +0300

    Исправление ошибок связанных с средним значением сумм аукциона

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # TGB-EUR-USD-ukrainian-rate
-This is telegram bot which parsing pages on minfin.com.ua, instead of using API
-### Bot has three buttons, each of which have private funtion. 
-  - First button has name - ("**_НБУ_**")
-  - Second button has name - ("**_Міжбанк_**")
-  - Third button has name - ("**_Аукціон_**")
+This is telegram bot who parsing pages on minfin.com.ua, instead of using API
+Bot have three buttons, each of which have private funtion. 
+First button has name - ("Rate EUR|USD National bank")
+Second button has name - ("Rate EUR|USD 
--- main.py
+++ main.py
@@ -54,210 +54,242 @@ def handle_mizhbank(message):
 
 @bot.message_handler(func=lambda message: message.text == 'Аукціон')
 def handle_auction(message):
+
+    sum_USD_buy = 0
+    sum_USD_sale = 0
+    sum_EUR_buy = 0
+    sum_EUR_sale = 0
+
+    count_USD_buy = 0
+    count_USD_sale = 0
+    count_EUR_buy = 0
+    count_EUR_sale = 0
+
     count = 0
     # Обработка нажатия кнопки "Аукціон"
     url_auction = 'https://minfin.com.ua/ua/currency/auction/usd/buy/kiev/'
     response_auction = requests.get(url_auction)
 
     auction_KIEV_USD_BUY_rate = (auction(response_auction, 0))
-    if auction_KIEV_USD_BUY_rate == 0: 
-        count = count + 1
+    if auction_KIEV_USD_BUY_rate != 0:
+        sum_USD_buy += auction_KIEV_USD_BUY_rate
+        count_USD_buy = count_USD_buy + 1
     auction_KIEV_USD_SALE_rate = (auction(response_auction, 1))
-    if auction_KIEV_USD_SALE_rate == 0: 
-        count = count + 1
+    if auction_KIEV_USD_SALE_rate != 0: 
+        sum_USD_sale += auction_KIEV_USD_SALE_rate
+        count_USD_sale = count_USD_sale + 1
 
     url_auction = 'https://minfin.com.ua/ua/currency/auction/eur/buy/kiev/'
     response_auction = requests.get(url_auction)
 
     auction_KIEV_EUR_BUY_rate =(auction(response_auction, 0))
-    if auction_KIEV_EUR_BUY_rate == 0: 
-        count = count + 1
+    if auction_KIEV_EUR_BUY_rate != 0: 
+        sum_EUR_buy += auction_KIEV_EUR_BUY_rate
+        count_EUR_buy = count_EUR_buy + 1
     auction_KIEV_EUR_SALE_rate = (auction(response_auction, 1))
-    if auction_KIEV_EUR_SALE_rate == 0: 
-        count = count + 1
+    if auction_KIEV_EUR_SALE_rate != 0: 
+        sum_EUR_sale += auction_KIEV_EUR_SALE_rate
+        count_EUR_sale = count_EUR_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/odessa/'
     response_auction = requests.get(url_auction)
 
     auction_ODESSA_USD_BUY_rate = (auction(response_auction, 0))
-    if auction_ODESSA_USD_BUY_rate == 0: 
-        count = count + 1
+    if auction_ODESSA_USD_BUY_rate != 0:
+        sum_USD_buy += auction_ODESSA_USD_BUY_rate
+        count_USD_buy = count_USD_buy + 1
     auction_ODESSA_USD_SALE_rate = (auction(response_auction, 1))
-    if auction_ODESSA_USD_SALE_rate == 0: 
-        count = count + 1
+    if auction_ODESSA_USD_SALE_rate != 0: 
+        sum_USD_sale += auction_ODESSA_USD_SALE_rate
+        count_USD_sale = count_USD_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/eur/buy/odessa/'
     response_auction = requests.get(url_auction)
 
     auction_ODESSA_EUR_BUY_rate = (auction(response_auction, 0))
-    if auction_ODESSA_EUR_BUY_rate == 0: 
-        count = count + 1
+    if auction_ODESSA_EUR_BUY_rate != 0: 
+        sum_EUR_buy += auction_ODESSA_EUR_BUY_rate
+        count_EUR_buy = count_EUR_buy + 1
     auction_ODESSA_EUR_SALE_rate = (auction(response_auction, 1))
-    if auction_ODESSA_EUR_SALE_rate == 0: 
-        count = count + 1
+    if auction_ODESSA_EUR_SALE_rate != 0: 
+        sum_EUR_sale += auction_ODESSA_EUR_SALE_rate
+        count_EUR_sale = count_EUR_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/lvov/'
     response_auction = requests.get(url_auction)
 
     auction_LVOV_USD_BUY_rate = (auction(response_auction, 0))
-    if auction_LVOV_USD_BUY_rate == 0: 
-        count = count + 1
+    if auction_LVOV_USD_BUY_rate != 0: 
+        sum_USD_buy += auction_LVOV_USD_BUY_rate
+        count_USD_buy = count_USD_buy + 1
     auction_LVOV_USD_SALE_rate = (auction(response_auction, 1))
-    if auction_LVOV_USD_SALE_rate == 0: 
-        count = count + 1
+    if auction_LVOV_USD_SALE_rate != 0: 
+        sum_USD_sale += auction_LVOV_USD_SALE_rate
+        count_USD_sale = count_USD_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/eur/buy/lvov/'
     response_auction = requests.get(url_auction)
 
     auction_LVOV_EUR_BUY_rate = (auction(response_auction, 0))
-    if auction_LVOV_EUR_BUY_rate == 0: 
-        count = count + 1
+    if auction_LVOV_EUR_BUY_rate != 0: 
+        sum_EUR_buy += auction_LVOV_EUR_BUY_rate
+        count_EUR_buy = count_EUR_buy + 1
     auction_LVOV_EUR_SALE_rate = (auction(response_auction, 1))
-    if auction_LVOV_EUR_SALE_rate == 0: 
-        count = count + 1
+    if auction_LVOV_EUR_SALE_rate != 0: 
+        sum_EUR_sale += auction_LVOV_EUR_SALE_rate
+        count_EUR_sale = count_EUR_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/kharkov/'
     response_auction = requests.get(url_auction)
 
     auction_KHARKOV_USD_BUY_rate = (auction(response_auction, 0))
-    if auction_KHARKOV_USD_BUY_rate == 0: 
-        count = count + 1
+    if auction_KHARKOV_USD_BUY_rate != 0:
+        sum_USD_buy += auction_KHARKOV_USD_BUY_rate
+        count_USD_buy = count_USD_buy + 1
     auction_KHARKOV_USD_SALE_rate = (auction(response_auction, 1))
-    if auction_KHARKOV_USD_SALE_rate == 0: 
-        count = count + 1
+    if auction_KHARKOV_USD_SALE_rate != 0: 
+        sum_USD_sale += auction_KHARKOV_USD_SALE_rate
+        count_USD_sale = count_USD_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/kharkov/'
     response_auction = requests.get(url_auction)
 
     auction_KHARKOV_EUR_BUY_rate = (auction(response_auction, 0))
-    if auction_KHARKOV_EUR_BUY_rate == 0: 
-        count = count + 1
+    if auction_KHARKOV_EUR_BUY_rate != 0: 
+        sum_EUR_buy += auction_KHARKOV_EUR_BUY_rate
+        count_EUR_buy = count_EUR_buy + 1
     auction_KHARKOV_EUR_SALE_rate = (auction(response_auction, 1))
-    if auction_KHARKOV_EUR_SALE_rate == 0: 
-        count = count + 1
+    if auction_KHARKOV_EUR_SALE_rate != 0: 
+        sum_EUR_sale += auction_KHARKOV_EUR_SALE_rate
+        count_EUR_sale = count_EUR_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/zaporozhye/'
     response_auction = requests.get(url_auction)
 
     auction_ZAPOROZHYE_USD_BUY_rate = (auction(response_auction, 0))
-    if auction_ZAPOROZHYE_USD_BUY_rate == 0: 
-        count = count + 1
+    if auction_ZAPOROZHYE_USD_BUY_rate != 0: 
+        sum_USD_buy += auction_ZAPOROZHYE_USD_BUY_rate
+        count_USD_buy = count_USD_buy + 1
     auction_ZAPOROZHYE_USD_SALE_rate = (auction(response_auction, 1))
-    if auction_ZAPOROZHYE_USD_SALE_rate == 0: 
-        count = count + 1
+    if auction_ZAPOROZHYE_USD_SALE_rate != 0:
+        sum_USD_sale += auction_ZAPOROZHYE_USD_SALE_rate
+        count_USD_sale = count_USD_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/zaporozhye/'
     response_auction = requests.get(url_auction)
 
     auction_ZAPOROZHYE_EUR_BUY_rate = (auction(response_auction, 0))
-    if auction_ZAPOROZHYE_EUR_BUY_rate == 0: 
-        count = count + 1
+    if auction_ZAPOROZHYE_EUR_BUY_rate != 0: 
+        sum_EUR_buy += auction_ZAPOROZHYE_EUR_BUY_rate
+        count_EUR_buy = count_EUR_buy + 1
     auction_ZAPOROZHYE_EUR_SALE_rate = (auction(response_auction, 1))
-    if auction_ZAPOROZHYE_EUR_SALE_rate == 0: 
-        count = count + 1
+    if auction_ZAPOROZHYE_EUR_SALE_rate != 0: 
+        sum_EUR_sale += auction_ZAPOROZHYE_EUR_SALE_rate
+        count_EUR_sale = count_EUR_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/chernigov/'
     response_auction = requests.get(url_auction)
 
     auction_CHERNIGOV_USD_BUY_rate = (auction(response_auction, 0))
-    if auction_CHERNIGOV_USD_BUY_rate == 0: 
-        count = count + 1
+    if auction_CHERNIGOV_USD_BUY_rate != 0: 
+        sum_USD_buy += auction_CHERNIGOV_USD_BUY_rate
+        count_USD_buy = count_USD_buy + 1
     auction_CHERNIGOV_USD_SALE_rate = (auction(response_auction, 1))
-    if auction_CHERNIGOV_USD_SALE_rate == 0: 
-        count = count + 1
+    if auction_CHERNIGOV_USD_SALE_rate != 0:
+        sum_USD_sale += auction_CHERNIGOV_USD_SALE_rate
+        count_USD_sale = count_USD_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/chernigov/'
     response_auction = requests.get(url_auction)
 
     auction_CHERNIGOV_EUR_BUY_rate = (auction(response_auction, 0))
-    if auction_CHERNIGOV_EUR_BUY_rate == 0: 
-        count = count + 1
+    if auction_CHERNIGOV_EUR_BUY_rate != 0: 
+        sum_EUR_buy += auction_CHERNIGOV_EUR_BUY_rate
+        count_EUR_buy = count_EUR_buy + 1
     auction_CHERNIGOV_EUR_SALE_rate = (auction(response_auction, 1))
-    if auction_CHERNIGOV_EUR_SALE_rate == 0: 
-        count = count + 1
+    if auction_CHERNIGOV_EUR_SALE_rate != 0: 
+        sum_EUR_sale += auction_CHERNIGOV_EUR_SALE_rate
+        count_EUR_sale = count_EUR_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/rovno/'
     response_auction = requests.get(url_auction)
 
     auction_ROVNO_USD_BUY_rate = (auction(response_auction, 0))
-    if auction_ROVNO_USD_BUY_rate == 0: 
-        count = count + 1
+    if auction_ROVNO_USD_BUY_rate != 0: 
+        sum_USD_buy += auction_ROVNO_USD_BUY_rate
+        count_USD_buy = count_USD_buy + 1
     auction_ROVNO_USD_SALE_rate = (auction(response_auction, 1))
-    if auction_ROVNO_USD_SALE_rate == 0: 
-        count = count + 1
+    if auction_ROVNO_USD_SALE_rate != 0:
+        sum_USD_sale += auction_ROVNO_USD_SALE_rate
+        count_USD_sale = count_USD_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/eur/buy/rovno/'
     response_auction = requests.get(url_auction)
 
     auction_ROVNO_EUR_BUY_rate = (auction(response_auction, 0))
-    if auction_ROVNO_EUR_BUY_rate == 0: 
-        count = count + 1
+    if auction_ROVNO_EUR_BUY_rate != 0: 
+        sum_EUR_buy += auction_ROVNO_EUR_BUY_rate
+        count_EUR_buy = count_EUR_buy + 1
     auction_ROVNO_EUR_SALE_rate = (auction(response_auction, 1))
-    if auction_ROVNO_EUR_SALE_rate == 0: 
-        count = count + 1
+    if auction_ROVNO_EUR_SALE_rate != 0: 
+        sum_EUR_sale += auction_ROVNO_EUR_SALE_rate
+        count_EUR_sale = count_EUR_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/vinnitsa/'
     response_auction = requests.get(url_auction)
 
     auction_VINNITSA_USD_BUY_rate = (auction(response_auction, 0))
-    if auction_VINNITSA_USD_BUY_rate == 0: 
-        count = count + 1
+    if auction_VINNITSA_USD_BUY_rate != 0: 
+        sum_USD_buy += auction_VINNITSA_USD_BUY_rate
+        count_USD_buy = count_USD_buy + 1
     auction_VINNITSA_USD_SALE_rate = (auction(response_auction, 1))
-    if auction_VINNITSA_USD_SALE_rate == 0: 
-        count = count + 1
+    if auction_VINNITSA_USD_SALE_rate != 0:
+        sum_USD_sale += auction_VINNITSA_USD_SALE_rate
+        count_USD_sale = count_USD_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/vinnitsa/'
     response_auction = requests.get(url_auction)
 
     auction_VINNITSA_EUR_BUY_rate = (auction(response_auction, 0))
-    if auction_VINNITSA_EUR_BUY_rate == 0: 
-        count = count + 1
+    if auction_VINNITSA_EUR_BUY_rate != 0: 
+        sum_EUR_buy += auction_VINNITSA_EUR_BUY_rate
+        count_EUR_buy = count_EUR_buy + 1
     auction_VINNITSA_EUR_SALE_rate = (auction(response_auction, 1))
-    if auction_VINNITSA_EUR_SALE_rate == 0: 
-        count = count + 1
+    if auction_VINNITSA_EUR_SALE_rate != 0: 
+        sum_EUR_sale += auction_VINNITSA_EUR_SALE_rate
+        count_EUR_sale = count_EUR_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/dnepropetrovsk/'
     response_auction = requests.get(url_auction)
 
     auction_DNEPROPETROVSK_USD_BUY_rate = (auction(response_auction, 0))
-    if auction_DNEPROPETROVSK_USD_BUY_rate == 0: 
-        count = count + 1
+    if auction_DNEPROPETROVSK_USD_BUY_rate != 0: 
+        sum_USD_buy += auction_DNEPROPETROVSK_USD_BUY_rate
+        count_USD_buy = count_USD_buy + 1
     auction_DNEPROPETROVSK_USD_SALE_rate = (auction(response_auction, 1))
-    if auction_DNEPROPETROVSK_USD_SALE_rate == 0: 
-        count = count + 1
+    if auction_DNEPROPETROVSK_USD_SALE_rate != 0: 
+        sum_USD_sale += auction_DNEPROPETROVSK_USD_SALE_rate
+        count_USD_sale = count_USD_sale + 1
 
     url_auction = 'https://minfin.com.ua/currency/auction/usd/buy/dnepropetrovsk/'
     response_auction = requests.get(url_auction)
 
     auction_DNEPROPETROVSK_EUR_BUY_rate = (auction(response_auction, 0))
-    if auction_DNEPROPETROVSK_EUR_BUY_rate == 0: 
-        count = count + 1
+    if auction_DNEPROPETROVSK_EUR_BUY_rate != 0:
+        sum_EUR_buy += auction_DNEPROPETROVSK_EUR_BUY_rate
+        count_EUR_buy = count_EUR_buy + 1
     auction_DNEPROPETROVSK_EUR_SALE_rate = (auction(response_auction, 1))
-    if auction_DNEPROPETROVSK_EUR_SALE_rate == 0: 
-        count = count + 1
+    if auction_DNEPROPETROVSK_EUR_SALE_rate != 0:
+        sum_EUR_sale += auction_DNEPROPETROVSK_EUR_SALE_rate
+        count_EUR_sale = count_EUR_sale + 1
 
-    middle_value_USD_buy = "{:.2f}".format(
-        (float(auction_KIEV_USD_BUY_rate) + float(auction_ODESSA_USD_BUY_rate) + float(auction_LVOV_USD_BUY_rate) +
-        float(auction_KHARKOV_USD_BUY_rate) + float(auction_ZAPOROZHYE_USD_BUY_rate) + float(auction_CHERNIGOV_USD_BUY_rate) +
-        float(auction_ROVNO_USD_BUY_rate) + float(auction_VINNITSA_USD_BUY_rate) + float(auction_DNEPROPETROVSK_USD_BUY_rate)) / (9 - count)
-    )
-    middle_value_USD_sale = "{:.2f}".format(
-        (float(auction_KIEV_USD_SALE_rate) + float(auction_ODESSA_USD_SALE_rate) + float(auction_LVOV_USD_SALE_rate) +
-        float(auction_KHARKOV_USD_SALE_rate) + float(auction_ZAPOROZHYE_USD_SALE_rate) + float(auction_CHERNIGOV_USD_SALE_rate) +
-        float(auction_ROVNO_USD_SALE_rate) + float(auction_VINNITSA_USD_SALE_rate) + float(auction_DNEPROPETROVSK_USD_SALE_rate)) / (9 - count)
-    )
+    middle_value_USD_buy = "{:.2f}".format(sum_USD_buy / count_USD_buy)
 
-    middle_value_EUR_buy = "{:.2f}".format(
-        (float(auction_KIEV_EUR_BUY_rate) + float(auction_ODESSA_EUR_BUY_rate) + float(auction_LVOV_EUR_BUY_rate) +
-        float(auction_KHARKOV_EUR_BUY_rate) + float(auction_ZAPOROZHYE_EUR_BUY_rate) + float(auction_CHERNIGOV_EUR_BUY_rate) +
-        float(auction_ROVNO_EUR_BUY_rate) + float(auction_VINNITSA_EUR_BUY_rate) + float(auction_DNEPROPETROVSK_EUR_BUY_rate)) / (9 - count)
-    )
+    middle_value_USD_sale = "{:.2f}".format(sum_USD_sale / count_USD_sale)
 
-    middle_value_EUR_sale = "{:.2f}".format(
-        (float(auction_KIEV_EUR_SALE_rate) + float(auction_ODESSA_EUR_SALE_rate) + float(auction_LVOV_EUR_SALE_rate) +
-        float(auction_KHARKOV_EUR_SALE_rate) + float(auction_ZAPOROZHYE_EUR_SALE_rate) + float(auction_CHERNIGOV_EUR_SALE_rate) +
-        float(auction_ROVNO_EUR_SALE_rate) + float(auction_VINNITSA_EUR_SALE_rate) + float(auction_DNEPROPETROVSK_EUR_SALE_rate)) / (9 - count)
-    )
+    middle_value_EUR_buy = "{:.2f}".format(sum_EUR_buy / count_EUR_buy)
+
+    middle_value_EUR_sale = "{:.2f}".format(sum_EUR_sale / count_EUR_sale)
     
     message_text = (
     "Аукціон\n<b><i>Київ</i></b>\n"
